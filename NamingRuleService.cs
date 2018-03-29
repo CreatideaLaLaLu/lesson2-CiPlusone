9bf87b0b675b18b6b68f746b36fbb0e5eeaa3d59
王證壹 <37131800+CiPlusone@users.noreply.github.com>
2018-03-29 11:46:54 +0800

    Add files via upload

---
+++ NamingRuleService.cs
@@ -0,0 +1,49 @@
+﻿namespace Yunlin.Culture.Video.Service
+{
+    using System.Text;
+
+    public class NamingRuleService
+    {
+        public int TestFunction(int index)
+        {
+            switch (index) {
+                case 1: break;
+                default: break;
+            }
+
+            if (index == 0) {
+                this.DoTrue();
+            }
+            else {
+                this.DoFalse();
+            }
+
+            if (this.DoTrue()) {
+                this.DoTrue();
+            }
+
+            return index * (index + 1) / 2;
+        }
+
+        public void TestStringFunction(string value)
+        {
+            var newString = "ABCDEFG"; // 新字串
+
+            // if ((value != null) | (value != string.Empty)) // 永遠為true
+            // value = newString.Replace("A", ",").Replace("B", "_").Replace("C", " ");
+            if (true) {
+                value = new StringBuilder(newString).Replace("A", ",").Replace("B", "_").Replace("C", " ").ToString();
+            }
+        }
+
+        private bool DoFalse()
+        {
+            return false;
+        }
+
+        private bool DoTrue()
+        {
+            return true;
+        }
+    }
+}
